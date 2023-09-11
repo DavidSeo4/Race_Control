c13d97218e4bd75f0669522b2ef2dcd93b20f57f
DavidSeo4 <seodavid4@gmail.com>
2023-09-11 08:45:11 +0200

    CarreraEstandar casi OK 2.0

--- src/main/java/com/campusdual/racecontrol/model/CarreraEstandar.java
+++ src/main/java/com/campusdual/racecontrol/model/CarreraEstandar.java
@@ -27,39 +27,11 @@ public class CarreraEstandar extends Carrera {
             //COMPROBACION DE SI EL NUMERO DE GARAJES DISPONIBLES DE LA CARRERA ES MAYOR QUE 1//
 
             if (this.getRegistroGarajes().size() < 2) {
-                int aleatorio2 = (int) Math.floor(Math.random() * ((this.getRegistroGarajes().size() - 1) - 0 + 1) + 0);
-
                 //OPCION 1 SOLO GARAJE DISPONIBLE//
-                System.out.println("La carrera se celebrará con el unico garaje disponible: " + this.getRegistroGarajes().get(aleatorio2).getPegatinaGaraje() + "\n Los coches participantes son:");
-                ArrayList<Coche> cochesParticipantes = this.getRegistroGarajes().get(aleatorio2).getCochesGaraje();
-                cochesParticipantes.forEach(elemento -> System.out.println(elemento.getModelo() +" con pegatina "+ elemento.getPegatinaCoche()));
-                System.out.println("=== Celebrando carrera... ===");
-                for (int minuto = 0; minuto <= (this.duracionHoras*60); minuto++) {
-                    for (Coche c : cochesParticipantes) {
-                        c.acelerar_frenar();
-                    }
-                }
-
-                // Ordenar cochesParticipantes utilizando el comparador al inicio del método//
-                cochesParticipantes.sort(comparador);
-                Collections.reverse(cochesParticipantes);
-                this.getClasificacionCarrera().addAll(cochesParticipantes);
-                System.out.println("=== Esta es la clasificacion de la carrera: ===");
-
-                for (int i = 0; i<this.getClasificacionCarrera().size(); i++){
-                    System.out.println((i+1) +"º. "+ getClasificacionCarrera().get(i).getModelo() +" con pegatina "+ getClasificacionCarrera().get(i).getPegatinaCoche());
-                }
-
-                this.setCarreraCelebrada(true);
-
-                System.out.println("El podio de la carrera es: \n 1º. " + this.getClasificacionCarrera().get(0).getModelo() + "\n 2º. " + this.getClasificacionCarrera().get(1).getModelo() + "\n 3º. " + this.getClasificacionCarrera().get(2).getModelo());
-                System.out.println("Se llevarán 10, 7 y 5 puntos respectivamente.");
-                this.getClasificacionCarrera().get(0).setPuntuacion(  this.getClasificacionCarrera().get(0).getPuntuacion() + 10);
-                this.getClasificacionCarrera().get(1).setPuntuacion(  this.getClasificacionCarrera().get(1).getPuntuacion() + 7);
-                this.getClasificacionCarrera().get(2).setPuntuacion(  this.getClasificacionCarrera().get(2).getPuntuacion() + 5);
+                carrera1Garaje(isTorneo,comparador, torneo);
 
             } else {
-                //OPCION VARIOS GARAJES DISPONIBLES//
+                //OPCION 2 VARIOS GARAJES DISPONIBLES//
 
                 //RANDOMIZACION DEL NUMERO DE GARAJES PARTICIPANTES DE LA CARRERA (1 O VARIOS)//
                 int aleatorio = (int) Math.floor(Math.random() * (2 - 1 + 1) + 1);
@@ -75,7 +47,7 @@ public class CarreraEstandar extends Carrera {
     }
 
     public void carrera1Garaje(boolean isTorneo, Comparator comparador, Torneo torneo){
-        //ALEATORIAMENTE PARTICIPA SOLO 1 GARAJE EN LA CARRERA//
+        //PARTICIPA SOLO 1 GARAJE EN LA CARRERA//
         int aleatorio2 = (int) Math.floor(Math.random() * ((this.getRegistroGarajes().size() - 1) - 0 + 1) + 0);
         System.out.println("La carrera se celebrará con un unico garaje: " + this.getRegistroGarajes().get(aleatorio2).getPegatinaGaraje() + "\n Los coches participantes son: ");
         ArrayList<Coche> cochesParticipantes = this.getRegistroGarajes().get(aleatorio2).getCochesGaraje();
@@ -126,7 +98,7 @@ public class CarreraEstandar extends Carrera {
     }
 
     public void carreraVariosGarajes(boolean isTorneo, Comparator comparador, Torneo torneo){
-        //ALEATORIAMENTE PARTICIPAN VARIOS GARAJES EN LA CARRERA//
+        //PARTICIPAN VARIOS GARAJES EN LA CARRERA//
         System.out.println("=== La carrera se celebrará con varios garajes: ===");
         this.getRegistroGarajes().forEach(e -> System.out.println("- " + e.getPegatinaGaraje()));
         ArrayList<Coche> cochesParticipantes = new ArrayList<>();
